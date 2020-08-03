4999084e03a24f7e58d80306c0f9c4f8ac0eecdb
AsylumRunner479 <54158974+AsylumRunner479@users.noreply.github.com>
2020-08-03 16:21:54 +1000

    changed version

--- Andrew 1/Assets/Scripts/CarHandler.cs
+++ Andrew 1/Assets/Scripts/CarHandler.cs
@@ -9,7 +9,7 @@ public class CarHandler : MonoBehaviour
     public GameObject self;
     public HingeJoint[] wheel;
     public WheelCollider[] wheelColliders;
-    public gameobject wheels;
+    
 
     // Update is called once per frame
     void Update()
@@ -23,40 +23,7 @@ public class CarHandler : MonoBehaviour
             wheelColliders[i].transform.rotation = rotationQ;
         }
         self.transform.Rotate(0, Input.GetAxis("Horizontal"), 0, 0);
-        if (!(Input.GetAxis("Vertical") <= 0))
-        {
-            for (int i = 0; i < wheel.Length; i++)
-            {
-                JointMotor tempJoint = wheel[i].motor;
-                tempJoint.targetVelocity = 1000f;
-                tempJoint.force += 1f;
-                wheel[i].motor = tempJoint;
-
-
-
-            }
-
-        }
-        else if (!(Input.GetAxis("Vertical") >= 0))
-        {
-            for (int i = 0; i < wheel.Length; i++)
-            {
-                JointMotor tempJoint = wheel[i].motor;
-                tempJoint.targetVelocity = -1000f;
-                tempJoint.force += 1f;
-                wheel[i].motor = tempJoint;
-            }
-
-        }
-        else
-        {
-            for (int i = 0; i < wheel.Length; i++)
-            {
-                JointMotor tempJoint = wheel[i].motor;
-                tempJoint.force = 0f;
-                wheel[i].motor = tempJoint;
-            }
-        }
+        
 
     }
 }
